2a27615fd9c4ae412d3ca38b12b2d3b5fefe63e4
Katie Hoffman <ktahoffman@gmail.com>
2014-11-22 00:53:13 -0500

    add comment about play()

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,7 +1,14 @@
 $( document ).ready(function() {
   
+
+  
   // create your click handler here
 
+
+
+  // the below code calls on the play function found in game-functions.js
+  // the play function calls on selectMole, then waits a second, then calls
+  // on selectMole, then waits a second, you get the idea :)
   play();
 
 });
